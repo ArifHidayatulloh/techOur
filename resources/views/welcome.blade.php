3dfc147e646d663840dc85c23decf07a1c30ab1c
HarnandaRafikaA <harnandarafika@gmail.com>
2023-09-25 19:02:09 +0700

    mengubah profile di home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
     <title>INDEX</title>
 </head>
@@ -32,7 +33,21 @@
                             </a>
                             <i class="fa-solid fa-circle-user fa-xl mt-2" style="color: #ffffff;"></i>
                             <ul class="dropdown-menu text-center" id="dropdown-menu">
-                                <li><a href="{{ route('profile') }}">Profile</a></li>
+                                <li>{{ Auth::user()->name }}</li>
+                                <li>
+                                    @if (Auth::user()->image == null)
+                                        <img src="{{ asset('assets/image/team.jpg') }}" alt=""
+                                            class="rounded-circle w-75 mt-2 mb-2">
+                                    @else
+                                        <img src="{{ asset('storage/' . Auth::user()->image) }}" alt=""
+                                            class="rounded-circle w-75 mt-2 mb-2">
+                                    @endif
+                                </li>
+                                <li>{{ Auth::user()->email }}</li>
+                                <li class="bg-body-tertiary mb-3 mt-3 p-1">
+                                    <a href="/password"><i class='bx bx-key bx-sm'></i></a>
+                                    <a href="/profile"><i class='bx bxs-edit-alt bx-sm'></i></a>
+                                </li>
                                 <li><a href="{{ route('actionlogout') }}">Log Out</a>
                                 </li>
                             </ul>
@@ -48,8 +63,8 @@
                         <div class="box">
                             <center><a href="{{ route('competition.index') }}" class="link">CATEGORY COMPETITION</a>
                             </center>
-                            <a href="{{ route('competition.index') }}"><img src="{{ 'assets/image/category-comp.png' }}"
-                                    alt=""></a>
+                            <a href="{{ route('competition.index') }}"><img
+                                    src="{{ 'assets/image/category-comp.png' }}" alt=""></a>
                         </div>
                         <div class="box">
                             <center><a href="{{ route('tournament.index') }}" class="link">TOURNAMENT</a></center>
@@ -77,14 +92,12 @@
                             <a href="{{ route('news.index') }}"><img src="{{ 'assets/image/news.png' }}"
                                     alt=""></a>
                         </div>
-                        @elseif(Auth::user() == null)
+                    @elseif(Auth::user() == null)
                         <script>
                             var info = 'Session telah habis, kembali login'
                             alert(info)
-                            document.location.href="{{ route('login') }}"
+                            document.location.href = "{{ route('login') }}"
                         </script>
-                        
-
                     @endif
 
                     @if (Auth::user()->role == 'admin')
